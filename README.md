b8b1965802456e6229c0e017f4a9c45f5003d021
wy <228574227@qq.com>
2024-10-11 10:46:11 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,2 @@
+## MinioJS
+Minio的官方客户端使用时必须依赖node.js, 无法在vite项目中使用, 此项目把minio的使用打包为纯js,可以在浏览器中直接调用minio的客户端
---
+++ src/index.js
@@ -0,0 +1,8 @@
+export const Minio = require("minio");
+
+
+//封装的上传到minio
+export function getMinio() {
+  return Minio
+}
+
